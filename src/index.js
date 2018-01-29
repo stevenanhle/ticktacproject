e579f0a8e65846c2338aa4324348bccb7e6aa458
Son Le <steven87.le@gmail.com>
2018-01-29 12:11:01 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+<div>
+    Hello World
+</div>
+, document.getElementById('root'));
+registerServiceWorker();
